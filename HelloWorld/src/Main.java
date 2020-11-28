6a07660ffca8ae524e4726fbfea060c395ff806f
Sean Murray <regina.wagner.us@gmail.com>
2020-11-27 20:00:44 -0700

    Add HelloWorld Project

---
+++ HelloWorld/src/Main.java
@@ -0,0 +1,9 @@
+/*
+    Here's my fancy shmacny comment.
+ */
+public class Main {
+    public static void main(String args[]) {
+        // This prints some text to the screen.
+        System.out.println("Hello World!");
+    }
+}
