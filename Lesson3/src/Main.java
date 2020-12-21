68ae62af54afca11f1acff498ae101ca375f360b
regina-ctrl <regina.wagner.us@gmail.com>
2020-12-20 20:02:56 -0500

    add Lesson3 Part2

--- Lesson3/src/Main.java
+++ Lesson3/src/Main.java
@@ -4,6 +4,12 @@ public class Main {
         // guessLetterGame1 ();
         //guessLetterGame3();
         //ifElseladder();
+        //switchDemo ();
+        //switchNoBreak ();
+        //sqrRoot();
+        //decreasingForLoop();
+        //commaForLoop();
+        sInputForTest();
     }
 
     private static void keyBoardInput() throws java.io.IOException {
@@ -57,6 +63,84 @@ public class Main {
             } else {
                 System.out.println("x is not between 1 and 4");
             }
+        }
+    }
+
+    public static void switchDemo() {
+        int i;
+        for(i=0; i<10; i++) {
+            switch (i) {
+                case 0:
+                    System.out.println("i is zero");
+                    break;
+                case 1:
+                    System.out.println("i is one");
+                    break;
+                case 2:
+                    System.out.println("i is two");
+                    break;
+                case 3:
+                    System.out.println("i is three");
+                    break;
+                case 4:
+                    System.out.println("i is four");
+                    break;
+                default:
+                    System.out.println("i is five or more");
+            }
+        }
+    }
+
+    public static void switchNoBreak() {
+        int i;
+        for(i=0; i<=5; i++) {
+            switch(i) {
+                case 0:
+                    System.out.println("i is less than one");
+                case 1:
+                    System.out.println("i is less than two");
+                case 2:
+                    System.out.println("i is less than three");
+                case 3:
+                    System.out.println("i is less than four");
+                case 4:
+                    System.out.println("i is less than five");
+            }
+            System.out.println();
+        }
+    }
+
+    public static void sqrRoot() {
+        double num, sroot, rerr;
+        for(num = 1.0; num < 100.0; num++) {
+            sroot = Math.sqrt(num);
+            System.out.println("Square root of " + num +
+            " is " + sroot);
+            // compute rounding error
+            rerr = num - (sroot * sroot);
+            System.out.println("Rounding error is " + rerr);
+            System.out.println();
+        }
+    }
+
+    public static void decreasingForLoop() {
+        int x;
+        for(x = 100; x > -100; x -= 5)
+            System.out.println(x);
+    }
+
+    public static void commaForLoop() {
+        int i, j;
+        for(i=0, j=10; i < j; i++, j--) {
+            System.out.println("i and j: " + i + " " + j);
+        }
+    }
+
+    public static void sInputForTest() throws java.io.IOException {
+        int i;
+        System.out.println("Press S to stop.");
+        for(i = 0; (char) System.in.read() != 'S'; i++) {
+            System.out.println("Pass #" + i);
         }
     }
 }
