0441f96859aae8be01e93f329dc78943ba5ed2d9
Sean Murray <palidorpaladin@gmail.com>
2021-01-18 23:28:47 -0700

    Added the rest of Lesson 3 homework.

--- Lesson3/src/Homework.java
+++ Lesson3/src/Homework.java
@@ -16,6 +16,21 @@ public class Homework {
         assert !isLowercaseLetter('A') : "isLowercaseLetter reported true, but character was 'A'";
         assert !isLowercaseLetter('%') : "isLowercaseLetter reported true, but character was '%'";
 
+        // Test for dayOfWeek
+        assert dayOfWeek(1) == "Monday" : "dayOfWeek returned a value other than Monday";
+        assert dayOfWeek(2) == "Tuesday" : "dayOfWeek returned a value other than Tuesday";
+        assert dayOfWeek(3) == "Wednesday" : "dayOfWeek returned a value other than Wednesday";
+        assert dayOfWeek(4) == "Thursday" : "dayOfWeek returned a value other than Thursday";
+        assert dayOfWeek(5) == "Friday" : "dayOfWeek returned a value other than Friday";
+        assert dayOfWeek(6) == "Saturday" : "dayOfWeek returned a value other than Saturday";
+        assert dayOfWeek(7) == "Sunday" : "dayOfWeek returned a value other than Sunday";
+        assert dayOfWeek(8) == "Invalid" : "dayOfWeek returned a value other than Invalid";
+        assert dayOfWeek(-1) == "Invalid" : "dayOfWeek returned a value other than Invalid";
+        assert dayOfWeek(0) == "Invalid" : "dayOfWeek returned a value other than Invalid";
+
+        printWarmTemperaturesInCelsius();
+        printTenToZeroButOnlyEvenNumber();
+
         System.out.println("All tests have passed successfully. Congratulations!");
     }
 
@@ -29,4 +44,47 @@ public class Homework {
         // TODO: Use an If-Else-If Ladder to show maximum understanding.
         return false;
     }
+
+    private static String dayOfWeek(int day) {
+        // TODO: Use a switch statement to return the name of the day.
+        //       Assume the first day of the week in 1.
+        //       Assume the day should be capitalized, e.g. "Monday"
+        //       If an invalid day is provided, the return value should be "Invalid"
+        return "";
+    }
+
+    private static void printWarmTemperaturesInCelsius() {
+        // TODO: Use a while loop to print temperatures from 25 degrees Celsius
+        //       down to 20 degree Celsius.
+        //       Output should be as follows:
+        //         "Temperature is <temp> degrees Celsius"
+        //         e.g. "Temperature is 20 degree Celsius"
+        //       NOTE: Watch out for infinite loops! You'll need to update
+        //             your control variable effectively. If you have an infinite
+        //             loop, press Ctrl+C to exit.
+    }
+
+    private static void printTenToZeroButOnlyEvenNumber() {
+        // TODO: Use a for loop to count from 10 down to 0, printing
+        //       only even numbers to the screen.
+        //       NOTE: The control value MUST count down by 1.
+        //       HINT: Use the continue keyword to skip values that shouldn't
+        //             be printed.
+    }
+
+    /*
+        Analyze the following for loop and describe what the output will be.
+
+        for (char Ch = ’Z’ ; Ch >= ’A’ ; --Ch) {
+            if ((Ch == ’A’) || (Ch == ’E’) || (Ch == ’I’) ||
+                (Ch == ’O’) || (Ch == ’U’)) {
+                System.out.println(Ch + " is a vowel.");
+            } else {
+                System.out.println(Ch + " is a consonant.");
+            }
+        }
+
+        Add your answer below here, within the comment block.
+        ------------------------------------------------------
+     */
 }
