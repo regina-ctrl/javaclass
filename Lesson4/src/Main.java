54a165d365d5349c60af5df0470bd3e205cf8b83
regina-ctrl <regina.wagner.us@gmail.com>
2021-01-24 15:33:46 -0500

    Lesson4 continued

--- Lesson4/src/Main.java
+++ Lesson4/src/Main.java
@@ -8,6 +8,11 @@ public class Main {
             int passengers;     // number of passengers
             int fuelcap;        // fuel capacity in gallons
             int mpg;            // fuel consumption in miles per gallon
+
+
+            int getRange() {
+                return this.fuelcap * this.mpg;
+            }
         }
 
         VehicleV1 myVehicle = new VehicleV1();
@@ -16,6 +21,27 @@ public class Main {
         myVehicle.passengers = 10;
         System.out.println(String.format("Passengers: [%d], MPG: [%d], Fuel Capacity: [%d]",
                 myVehicle.passengers, myVehicle.mpg, myVehicle.fuelcap));
+
+        System.out.println("---------------------------------");
+        System.out.println("--example of 2 vehicle variables (pointers) pointing to the same object--");
+        VehicleV1 mySecondVehicle = myVehicle;
+        myVehicle.passengers = 20;
+        System.out.println(String.format("Passengers: [%d], MPG: [%d], Fuel Capacity: [%d]",
+                myVehicle.passengers, myVehicle.mpg, myVehicle.fuelcap));
+        System.out.println(String.format("Passengers: [%d], MPG: [%d], Fuel Capacity: [%d]",
+                mySecondVehicle.passengers, mySecondVehicle.mpg, mySecondVehicle.fuelcap));
+        System.out.println("---------------------------------");
+
+        VehicleV1 sportsCar = new VehicleV1();
+        sportsCar.fuelcap = 10;
+        sportsCar.mpg = 5;
+        sportsCar.passengers = 2;
+        System.out.println(String.format("My sportscar has Passengers: [%d], MPG: [%d], Fuel Capacity: [%d]",
+                sportsCar.passengers, sportsCar.mpg, sportsCar.fuelcap));
+
+        int range = sportsCar.fuelcap * sportsCar.mpg;
+        System.out.println(String.format("My sportscar range: [%d]", range));
+        System.out.println(String.format("My sportscar range with getRange method: [%d]", sportsCar.getRange()));
     }
 }
 
